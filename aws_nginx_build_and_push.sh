a66036b4c7102acc484938c3d1dbcc5b99e82938
mulonemartin <mulone.martin@gmail.com>
2024-01-16 15:16:27 -0300

    nginx configuration

--- aws_nginx_build_and_push.sh
+++ aws_nginx_build_and_push.sh
@@ -45,7 +45,7 @@ if [ -z "${e}" ] || [ -z "${i}" ] || [ -z "${r}" ]; then
     usage
 fi
 
-docker image build -t stable-protocol-api-v2_$ENV -f ./Dockerfile.nginx .
+docker image build -t stable-protocol-api-v2-nginx_$ENV -f ./Dockerfile.nginx .
 
 echo "Build done!"
 
@@ -54,8 +54,8 @@ $(aws ecr get-login --no-include-email --region $AWS_REGION)
 
 echo "Logging to AWS done!"
 
-docker tag stable-protocol-api-v2_$ENV:latest $AWS_ID.dkr.ecr.$AWS_REGION.amazonaws.com/stable-protocol-api-v2_$ENV:latest
+docker tag stable-protocol-api-v2-nginx_$ENV:latest $AWS_ID.dkr.ecr.$AWS_REGION.amazonaws.com/stable-protocol-api-v2-nginx_$ENV:latest
 
-docker push $AWS_ID.dkr.ecr.$AWS_REGION.amazonaws.com/stable-protocol-api-v2_$ENV:latest
+docker push $AWS_ID.dkr.ecr.$AWS_REGION.amazonaws.com/stable-protocol-api-v2-nginx_$ENV:latest
 
 echo "finish done!"
