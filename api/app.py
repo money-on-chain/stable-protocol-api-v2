82b66372c18549154b4f22df31211377498612f4
mulonemartin <mulone.martin@gmail.com>
2024-08-22 10:43:39 -0300

    fix: bump version

--- api/app.py
+++ api/app.py
@@ -12,7 +12,7 @@ from api.logger import log
 from api.db import connect_and_init_db, close_db_connect
 
 
-API_VERSION = '1.0.6'
+API_VERSION = '1.0.7'
 API_TITLE = 'Stable Protocol API v2'
 API_DESCRIPTION = """
 This is a requirement for [stable-protocol-interface-v2](https://github.com/money-on-chain/stable-protocol-interface-v2)
